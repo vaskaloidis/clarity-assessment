2547665cf68e57f8fb8bf1db319fb5f11786ac36
vaskaloidis <vas.kaloidis@gmail.com>
2019-06-10 02:53:12 -0400

    Deleted old comments

--- src/main/java/comparator/AircraftComparator.java
+++ src/main/java/comparator/AircraftComparator.java
@@ -13,17 +13,13 @@ public class AircraftComparator implements Comparator<Aircraft> {
     @Override
     public int compare(Aircraft first, Aircraft second) {
 
+        // Algorithm
         // 1. Passenger AC > Cargo AC
         // 2. Large AC > Small AC (Same Type)
         // 3. Earlier enqueued AC > Later Enqueued (Same Type + Size)
 
-//        LocalDate firstDate = first.getQueueTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//        LocalDate secondDate = first.getQueueTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-
         if(first.getType().equals(second.getType())){
             if(first.getSize().equals(second.getSize())) {
-//                if(firstDate.isBefore(secondDate)) {
                 if(first.getQueueTime() < second.getQueueTime()) {
                     return 1;
                 } else {
@@ -39,16 +35,6 @@ public class AircraftComparator implements Comparator<Aircraft> {
         } else {
             return -1;
         }
-
-//        if(!first.getType().equals(second.getType()) && first.getType().equals(AircraftType.PASSENGER)) {
-//            return 3;
-//        } else if(first.getSize().equals(second.getSize()) && first.getSize().equals(AircraftSize.LARGE)) {
-//            return 2;
-//        } else if(((first.getType().equals(second.getType())) && (first.getSize().equals(second.getSize()))) && firstDate.isAfter(secondDate)) {
-//            return 1;
-//        } else {
-//            return -1;
-//        }
     }
 
 }
